166deb73e6835ad2f73448780048a40a17c6f488
Ivan Shadrin <chubakaivan@gmail.com>
2021-10-19 16:56:02 +0300

    ...

--- src/validation/validation.js
+++ src/validation/validation.js
@@ -30,25 +30,3 @@ const useSchema = () => {
 
 export default useSchema;
 
-// const loginSchema = Yup.object().shape({
-//   username: Yup.string()
-//     .min(5, 'ляляля')
-//     .required('Обязательно к заполнению'),
-//   password: Yup.string()
-//     .min(5, 'Слишком короткий пароль!')
-//     .required('Обязательно к заполнению'),
-// });
-
-// const signupSchema = Yup.object().shape({
-//   username: Yup.string()
-//     .min(5, 'Слишком короткий логин!')
-//     .matches('^[a-zA-Z0-9]+$', 'Логин должен содержать только буквы и цифры')
-//     .required('Обязательно к заполнению'),
-//   password: Yup.string()
-//     .min(5, 'Слишком короткий пароль!')
-//     .required('Обязательно к заполнению'),
-//   confimpass: Yup.string()
-//     .oneOf([Yup.ref('password')], 'Пароли не совпадают').required('Обязательно к заполнению'),
-// });
-
-// export { loginSchema, signupSchema };
